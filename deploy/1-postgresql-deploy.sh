7e8b042c849b51eb968d26c72116f32473006f33
Romanov R. <rr@pentestit.ru>
2024-09-09 15:56:05 +0300

    deploy/1-postgresql-deploy.sh

--- deploy/1-postgresql-deploy.sh
+++ deploy/1-postgresql-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Settings
 nw_api_password=""
 nw_cabinet_password=""
 
@@ -7,10 +8,10 @@ nw_cabinet_password=""
 os_base=$(cat /etc/os-release | grep -E '^ID=' | awk '{print $2}' FS="=" | tr -d '"' | head -c 1)
 os_vers=$(cat /etc/os-release | grep -E '^VERSION_ID=' | awk '{print $2}' FS="=" | tr -d '"' | awk '{print $1}' FS=".")
 
-## Set netstat
-if [ "$os_base" == "f" ]; then netstat="sockstat -4l"; else netstat="netstat -nlp"; fi
+## Netstat
+netstat="netstat -nlp"
 
-## Environment udpate
+## Update
 if [[ "$os_base" == "d"  || "$os_base" == "u" ]]
 then
   (apt update > /dev/null 2>&1) || (apt upgrade -y > /dev/null 2>&1)
@@ -19,14 +20,6 @@ then
   setenforce 0 > /dev/null 2>&1
   echo -e "SELINUX=enforcing\nSELINUX=disabled" > /etc/selinux/config
   dnf update -y > /dev/null 2>&1
-fi
-
-if [ $? -eq 0 ]
-then
-  echo "Environment successfully updated"
-else
-  echo "Error occure while updating"
-  exit 1
 fi
 
 ## PostgreSQL install
@@ -45,15 +38,10 @@ then
   ($netstat | grep -q ':5432')
 fi
 
-if [ $? -eq 0 ]
-then
-  echo "PostgreSQL successfully installed"
-else
-  echo "Error occure while install PostgreSQL"
-  exit 1
-fi
+##
+# Create databases
+##
 
-# Install DBMS
 su - postgres -c "psql -c \"CREATE DATABASE waf;\"" > /dev/null 2>&1
 su - postgres -c "psql -c \"CREATE ROLE nw_api PASSWORD '$nw_api_password';\"" > /dev/null 2>&1
 su - postgres -c "psql -c \"GRANT ALL ON DATABASE waf TO nw_api;\"" > /dev/null 2>&1
@@ -62,12 +50,7 @@ su - postgres -c "psql waf -c \"GRANT ALL ON ALL TABLES IN SCHEMA public TO nw_a
 su - postgres -c "psql waf -c \"GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nw_api;\"" > /dev/null 2>&1
 su - postgres -c "psql waf -c \"GRANT CREATE ON SCHEMA public TO nw_api;\"" > /dev/null 2>&1
 
-if [ $? -eq 0 ]
-then
-  echo "Database \"waf\" successfully created"
-else
-  echo "Error occure while create database"
-fi
+#
 
 su - postgres -c "psql -c \"CREATE DATABASE cabinet;\"" > /dev/null 2>&1
 su - postgres -c "psql -c \"CREATE ROLE nw_cabinet PASSWORD '$nw_cabinet_password';\"" > /dev/null 2>&1
@@ -76,10 +59,3 @@ su - postgres -c "psql -c \"ALTER ROLE nw_cabinet WITH LOGIN;\"" > /dev/null 2>&
 su - postgres -c "psql cabinet -c \"GRANT ALL ON ALL TABLES IN SCHEMA public TO nw_cabinet;\"" > /dev/null 2>&1
 su - postgres -c "psql cabinet -c \"GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nw_cabinet;\"" > /dev/null 2>&1
 su - postgres -c "psql cabinet -c \"GRANT CREATE ON SCHEMA public TO nw_cabinet;\"" > /dev/null 2>&1
-
-if [ $? -eq 0 ]
-then
-  echo "Database \"cabinet\" successfully created"
-else
-  echo "Error occure while create database"
-fi
