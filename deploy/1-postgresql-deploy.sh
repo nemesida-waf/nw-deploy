ce862031a817279ea1fd2da5f4ab69c306957fc1
Hekp0 <95239339+Hekp0@users.noreply.github.com>
2024-09-10 13:12:38 +0300

    Update 1-postgresql-deploy.sh

--- deploy/1-postgresql-deploy.sh
+++ deploy/1-postgresql-deploy.sh
@@ -1,95 +1,64 @@
 #!/bin/bash
 
-#for i in "$@"
-#do
-#case $i in
-#    nw_api_password="$2")
-#    nw_api_password="";
-#    shift
-#    ;;
-#    nw_cabinet_password=)
-#    nw_cabinet_password="${i#*=}"
-#    ;;
-#    API_server_IP=)
-#    API_server_IP="${i#*=}"
-#    ;;
-#    *)
-#    ;;
-#esac
-#done
-
-
-while [ "$#" -gt 0 ]; do
-  case "$1" in
-    nw_api_password) nw_api_password="${1#*=}"; shift 1;;
-    nw_cabinet_password) nw_cabinet_password="${1#*=}"; shift 1;;
-    API_server_IP) API_server_IP="${1#*=}"; shift 1;;
-    nw_api_password|nw_cabinet_password|API_server_IP) echo "$1 requires an argument" >&2; exit 1;;
-    
-    -*) echo "unknown option: $1" >&2; exit 1;;
-    *) handle_argument "$1"; shift 1;;
+## Settings
+for i in "$@"; do
+  case $i in
+    -nw_api_password=*)
+      nw_api_password="${i#*=}"
+      shift
+      ;;
+    -nw_cabinet_password=*)
+      nw_cabinet_password="${i#*=}"
+      shift
+      ;;
+    -API_server=*)
+      API_server="${i#*=}"
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
   esac
 done
 
-
-
-#while [[ "$#" -gt 0 ]]; do
-#    case $1 in
-#        nw_api_password=*) nw_api_password="$2" ; shift ;;
-#        nw_cabinet_password=*) nw_cabinet_password="$3" ; shift;;
-#        API_server_IP=*) API_server_IP="$4" ;;
-#        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-#    esac
-#    shift
-#done
-
-echo "nw_api_password parameter is missed"
-echo "nw_cabinet_password parameter is missed"
-echo "Nemesida WAF API server IP is missed"
-
-read -t 30 -p "Enter a nw_api user password: " nw_api_password
-read -t 30 -p "Enter a nw_cabinet user password: " nw_cabinet_password
-read -t 30 -p "Enter a Nemesida WAF API server IP: " API_server_IP
-
-echo $2
-echo $3
-echo $4
-## Settings
+if [ -z "$nw_api_password" ]; then read -t 30 -p "Enter a nw_api user password: " nw_api_password ; fi
+if [ -z "$nw_cabinet_password" ]; then read -t 30 -p "Enter a nw_cabinet user password: " nw_cabinet_password ; fi
+if [ -z "$API_server" ]; then read -t 30 -p "Enter a Nemesida WAF API server IP: " API_server ; fi
 
 ## OS detection
-os_base=$(cat /etc/os-release | grep -E '^ID=' | awk '{print $2}' FS="=" | tr -d '"' | head -c 1)
-os_vers=$(cat /etc/os-release | grep -E '^VERSION_ID=' | awk '{print $2}' FS="=" | tr -d '"' | awk '{print $1}' FS=".")
-
-## Netstat
-netstat="netstat -nlp"
+os_base=$(cat /etc/os-release | grep -E '^ID=' | awk '{print $2}' FS="=")
+postgres_version=$(dpkg -l | grep -E 'postgres' | head -n 1 | awk '{print $3}' | cut -b 1-2)
 
 ## Update
-if [[ "$os_base" == "d"  || "$os_base" == "u" ]]
+if [[ "$os_base" == "debian"  || "$os_base" == "ubuntu" ]]
 then
-  (apt-get update > /dev/null 2>&1) || (echo "An error occure while update system"; exit 1)
-  (apt-get upgarde -qqy) || (echo "An error occure while upgrade system"; exit 1)
-elif [[ "$os_base" == "c"  || "$os_base" == "r" ]]
+  (apt-get update -qq && apt-get upgrade -qqy) || (echo -e "\033[0;101mAn error occured while update system\033[0m"; exit 1)
+elif [[ "$os_base" == "centos"  || "$os_base" == "red hat" ]]
 then
   setenforce 0 > /dev/null 2>&1
   echo -e "SELINUX=disabled\nSELINUXTYPE=targeted" > /etc/selinux/config
-  (dnf update -qqy) || (echo "Error occure while upgrade system"; exit 1)
+  (dnf update -qqy) || (echo -e "\033[0;101mAn error occured while upgrade system\033[0m"; exit 1)
 fi
 
 ## PostgreSQL install
-if [[ "$os_base" == "d"  || "$os_base" == "u" ]]
+if [[ "$os_base" == "debian"  || "$os_base" == "ubuntu" ]]
 then
-  (apt-get install postgresql -qqy) ||  (echo "An error occure while installing PostgreSQL"; exit 1)
-  systemctl reenable postgresql > /dev/null 2>&1
-  ($netstat | grep -q ':5438') || (echo "An error occure while starting PostgreSQL"; exit 1)
-elif [[ "$os_base" == "c"  || "$os_base" == "r" ]]
+  (apt-get install postgresql -qqy) ||  (echo -e "\033[0;101mAn error occured while installing PostgreSQL\033[0m"; exit 1)
+  sed -i "/# IPv4 local connections:/ a \host all all $API_server md5" /etc/postgresql/$postgres_version/main/pg_hba.conf
+elif [[ "$os_base" == "centos"  || "$os_base" == "red hat" ]]
 then
-  (dnf install postgresql-devel postgresql-server -qqy) || (echo "An error occure while installing PostgreSQL"; exit 1)
-  (postgresql-setup initdb) || (echo "An error occurred while initializing PostgreSQL"; exit 1)
-  sed -i "s|host all all 127.0.0.1/32 ident|host all all 127.0.0.1/32 md5|" /var/lib/pgsql/data/pg_hba.conf
-  sed -i "s|host all all ::1/128 ident|host all all ::1/128 md5|" /var/lib/pgsql/data/pg_hba.conf
-  systemctl reenable postgresql > /dev/null 2>&1
-  ($netstat | grep -q ':5432') || (echo "Error occure while starting PostgreSQL"; exit 1)
+  (dnf install postgresql-devel postgresql-server -qqy) || (echo -e "\033[0;101mAn error occured while installing PostgreSQL\033[0m"; exit 1)
+  (postgresql-setup initdb) || (echo -e "\033[0;101mAn error occurred while initializing PostgreSQL\033[0m"; exit 1)
+  sed -i -r 's|host\s+all\s+all\s+127.0.0.1/32\s+ident|host all all 127.0.0.1/32 md5|' /var/lib/pgsql/data/pg_hba.conf
+  sed -i -r 's|host\s+all\s+all\s+::1/128\s+ident|host all all ::1/128 md5|' /var/lib/pgsql/data/pg_hba.conf
+  sed -i "/# IPv4 local connections:/ a \host all all $API_server md5" /var/lib/pgsql/data/pg_hba.conf
 fi
+
+systemctl reenable postgresql > /dev/null 2>&1
+(netstat -lnp | grep -q ':5432') || (echo -e "\033[0;101mAn error occured while starting PostgreSQL\033[0m"; exit 1)
 
 ##
 # Create databases
@@ -112,4 +81,3 @@ su - postgres -c "psql -c \"ALTER ROLE nw_cabinet WITH LOGIN;\""
 su - postgres -c "psql cabinet -c \"GRANT ALL ON ALL TABLES IN SCHEMA public TO nw_cabinet;\""
 su - postgres -c "psql cabinet -c \"GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nw_cabinet;\""
 su - postgres -c "psql cabinet -c \"GRANT CREATE ON SCHEMA public TO nw_cabinet;\""
-
