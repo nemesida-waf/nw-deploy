f8af667324dfa70c8902d2b99f90a6653ecd8ce7
Hekp0 <95239339+Hekp0@users.noreply.github.com>
2024-09-16 15:35:30 +0300

    Update 1-postgresql-deploy.sh

--- deploy/1-postgresql-deploy.sh
+++ deploy/1-postgresql-deploy.sh
@@ -2,13 +2,14 @@
 
 ##
 # Example of use:
-# /bin/bash ./1-postgresql-deploy.sh 'pg_api_pwd=xxx' 'pg_cabinet_pwd=xxx' 'api_srv_ip=x.x.x.x'
+# /bin/bash ./1-api-deploy.sh 'pg_srv_ip=xxx' 'pg_srv_port=xxx' 'pg_api_pwd=x.x.x.x' 'api_proxy=xxx:xx'
 ##
 
 ## OS detection
 os_base=$(cat /etc/os-release | grep -E '^ID=' | awk '{print $2}' FS="=" | tr -d '"')
+os_version=$(cat /etc/os-release | grep -E '^VERSION_ID=' | awk '{print $2}' FS="=" | tr -d '"' | grep -o '^[^.]*')
+os_code_name=$(cat /etc/os-release | grep -E '^VERSION_CODENAME=' | awk '{print $2}' FS="=" | tr -d '"')
 
-## Check for supported OS
 if ! [[ "$os_base" =~ debian|ubuntu|rhel|centos|rocky ]]
 then
   echo -e "\033[0;101mUnsupported operating system. Please, contact us: info@nemesida-waf.com\033[0m"
@@ -18,16 +19,20 @@ fi
 ## Processing the params
 for i in "$@"; do
   case $i in
+    pg_srv_ip=*)
+      pg_srv_ip="${i#*=}"
+      shift
+      ;;
+    pg_srv_port=*)
+      pg_srv_port="${i#*=}"
+      shift
+      ;;
     pg_api_pwd=*)
       pg_api_pwd="${i#*=}"
       shift
       ;;
-    pg_cabinet_pwd=*)
-      pg_cabinet_pwd="${i#*=}"
-      shift
-      ;;
-    api_srv_ip=*)
-      api_srv_ip="${i#*=}"
+    api_proxy=*)
+      api_proxy="${i#*=}"
       shift
       ;;
     *)
@@ -36,14 +41,15 @@ for i in "$@"; do
 done
 
 ## Parameters validation
+if [ -z "$pg_srv_ip" ]; then echo -e "\033[0;101mERROR: pg_srv_ip parameter is missing\033[0m" ; exit 1 ; fi
+if [ -z "$pg_srv_port" ]; then echo -e "\033[0;101mERROR: pg_srv_port parameter is missing\033[0m" ; exit 1 ; fi
 if [ -z "$pg_api_pwd" ]; then echo -e "\033[0;101mERROR: pg_api_pwd parameter is missing\033[0m" ; exit 1 ; fi
-if [ -z "$pg_cabinet_pwd" ]; then echo -e "\033[0;101mERROR: pg_cabinet_pwd parameter is missing\033[0m" ; exit 1 ; fi
-if [ -z "$api_srv_ip" ]; then echo -e "\033[0;101mERROR: api_srv_ip parameter is missing\033[0m" ; exit 1 ; fi
 
 ## Display the applied parameters
+echo "PostgreSQL IP: $pg_srv_ip"
+echo "PostgreSQL port: $pg_srv_port"
 echo "Database password for user nw_api: $pg_api_pwd"
-echo "Database password for user nw_cabinet: $pg_cabinet_pwd"
-echo "Nemesida WAF API IP: $api_srv_ip"
+echo "Nemesida WAF API proxy (if used): $api_proxy"
 
 ## Parameters confirmation
 while [ "$ask" != "y" ]
@@ -51,6 +57,47 @@ do
   read -p "Continue? [y/n]: " ask
   ask=$(echo $ask | tr '[:upper:]' '[:lower:]')
 done
+
+##
+# Nemesida WAF repository
+##
+
+echo "Add Nemesida WAF repository"
+
+if [[ "$os_base" == debian ]]
+then
+  apt-get update -qqy
+  apt-get install apt-transport-https gnupg2 curl -qqy
+  if [[ "$os_code_name" == bullseye ]]
+  then
+    echo "deb https://nemesida-security.com/repo/nw/debian $os_code_name non-free" > /etc/apt/sources.list.d/NemesidaWAF.list
+  elif [[ "$os_code_name" == bookworm ]]
+  then
+    echo "deb https://nemesida-security.com/repo/nw/debian $os_code_name nwaf" > /etc/apt/sources.list.d/NemesidaWAF.list
+  fi
+  curl -s https://nemesida-security.com/repo/nw/gpg.key | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/trusted.gpg --import
+  chmod 644 /etc/apt/trusted.gpg.d/trusted.gpg
+  apt-get update -qqy
+elif [[ "$os_base" == ubuntu ]]
+then
+  apt-get update -qqy
+  apt-get install apt-transport-https gnupg2 curl -qqy
+  if [[ "$os_code_name" =~ focal|jammy ]]
+  then
+    echo "deb [arch=amd64] https://nemesida-security.com/repo/nw/ubuntu $os_code_name non-free" > /etc/apt/sources.list.d/NemesidaWAF.list
+  elif [[ "$os_code_name" == noble ]]
+  then
+    echo "deb [arch=amd64] https://nemesida-security.com/repo/nw/ubuntu $os_code_name nwaf" > /etc/apt/sources.list.d/NemesidaWAF.list
+  fi
+  curl -s https://nemesida-security.com/repo/nw/gpg.key | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/trusted.gpg --import
+  chmod 644 /etc/apt/trusted.gpg.d/trusted.gpg
+  apt-get update -qqy
+elif [[ "$os_base" =~ rhel|centos|rocky ]]
+then
+  echo -e "[NemesidaWAF]\nname=Nemesida WAF Packages for RHEL\nbaseurl=https://nemesida-security.com/repo/nw/rhel/\$releasever/\$basearch/\ngpgkey=https://nemesida-security.com/repo/nw/gpg.key\nenabled=1\ngpgcheck=1" > /etc/yum.repos.d/NemesidaWAF.repo
+  dnf install epel-release -qqy
+  dnf update -qqy
+fi
 
 ##
 # System update
@@ -69,57 +116,47 @@ then
 fi
 
 ##
-# PostgreSQL
+# Nemesida WAF API
 ##
 
-echo "Setting up PostgreSQL"
+echo "Setting up Nemesida WAF API"
 
-if [[ "$os_base" =~ debian|ubuntu ]]
+if [[ "$os_base" == debian ]]
 then
-  (apt-get install postgresql -qqy) ||  (echo -e "\033[0;101mERROR: install PostgreSQL is failed\033[0m"; exit 1)
+  apt-get install nginx python3-pip python3-dev postgresql-server-dev-all python3-venv memcached -qqy
+  apt-get install nwaf-api -qqy
+elif [[ "$os_base" == ubuntu ]]
+then
+  if [[ "$os_code_name" == focal ]]
+  then
+    apt-get install nginx python3.9 python3-pip python3.9-dev postgresql-server-dev-all python3.9-venv build-essential memcached -qqy
+  elif [[ "$os_code_name" =~ jammy|noble ]]
+  then
+    apt-get install nginx python3 python3-pip python3-dev postgresql-server-dev-all python3-venv build-essential memcached -qqy
+  fi
+  apt-get install nwaf-api -qqy
 elif [[ "$os_base" =~ rhel|centos|rocky ]]
 then
-  (dnf install postgresql-devel postgresql-server -qqy) || (echo -e "\033[0;101mERROR: install PostgreSQL is failed\033[0m"; exit 1)
-  (postgresql-setup initdb) || (echo -e "\033[0;101mERROR: PostgreSQL initialization is failed\033[0m"; exit 1)
+  dnf install epel-release -qqy
+  dnf update -qqy
+  if [[ "$os_version" == 8 ]]
+  then
+    dnf install nginx python39 python39-devel python39-setuptools python39-pip postgresql-devel gcc memcached -qqy
+  elif [[ "$os_version" == 9 ]]
+  then
+    dnf install nginx python3 python3-devel python3-setuptools python3-pip postgresql-devel gcc memcached -qqy
+  fi
+  dnf install nwaf-api -qqy
 fi
 
-postgres_version=$(ls /usr/lib/postgresql/ | grep -P '^\d+$' | sort -n | tail -1)
-sleep 10
+## Configure Nemesida WAF API settings file
+sed -i "s|DB_HOST = '127.0.0.1'|DB_HOST = '$pg_srv_ip'|" /var/www/nw-api/settings.py
+sed -i "s|DB_PORT = '5432'|DB_PORT = '$pg_srv_port'|" /var/www/nw-api/settings.py
+sed -i "s|DB_PASS = ''|DB_PASS = '$pg_api_pwd'|" /var/www/nw-api/settings.py
+sed -i "s|PROXY = ''|PROXY = '$api_proxy'|" /var/www/nw-api/settings.py
 
-if [[ "$os_base" =~ debian|ubuntu ]]
-then
-  cat /etc/postgresql/$postgres_version/main/pg_hba.conf | grep -q "host all all $api_srv_ip/32 md5" || sed -i "/# IPv4 local connections:/a host all all $api_srv_ip/32 md5" /etc/postgresql/$postgres_version/main/pg_hba.conf
-elif [[ "$os_base" =~ rhel|centos|rocky ]]
-then
-  sed -i -r 's|host\s+all\s+all\s+127.0.0.1/32\s+ident|host all all 127.0.0.1/32 md5|' /var/lib/pgsql/data/pg_hba.conf
-  sed -i -r 's|host\s+all\s+all\s+::1/128\s+ident|host all all ::1/128 md5|' /var/lib/pgsql/data/pg_hba.conf
-  cat /var/lib/pgsql/data/pg_hba.conf | grep -q "host all all $api_srv_ip/32 md5" || sed -i "/# IPv4 local connections:/a host all all $api_srv_ip/32 md5" /var/lib/pgsql/data/pg_hba.conf
-fi
-
-systemctl reenable postgresql
-systemctl start postgresql
-(netstat -lnp | grep -q ':5432') || (echo -e "\033[0;101mERROR: start PostgreSQL is failed\033[0m"; exit 1)
-
-##
-# Create the databases
-##
-
-echo "Creating databases"
-
-su - postgres -c "psql -c \"CREATE DATABASE waf;\""
-su - postgres -c "psql -c \"CREATE ROLE nw_api PASSWORD '$pg_api_pwd';\""
-su - postgres -c "psql -c \"GRANT ALL ON DATABASE waf TO nw_api;\""
-su - postgres -c "psql -c \"ALTER ROLE nw_api WITH LOGIN;\""
-su - postgres -c "psql waf -c \"GRANT ALL ON ALL TABLES IN SCHEMA public TO nw_api;\""
-su - postgres -c "psql waf -c \"GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nw_api;\""
-su - postgres -c "psql waf -c \"GRANT CREATE ON SCHEMA public TO nw_api;\""
-
-#
-
-su - postgres -c "psql -c \"CREATE DATABASE cabinet;\""
-su - postgres -c "psql -c \"CREATE ROLE nw_cabinet PASSWORD '$pg_cabinet_pwd';\""
-su - postgres -c "psql -c \"GRANT ALL ON DATABASE cabinet TO nw_cabinet;\""
-su - postgres -c "psql -c \"ALTER ROLE nw_cabinet WITH LOGIN;\""
-su - postgres -c "psql cabinet -c \"GRANT ALL ON ALL TABLES IN SCHEMA public TO nw_cabinet;\""
-su - postgres -c "psql cabinet -c \"GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO nw_cabinet;\""
-su - postgres -c "psql cabinet -c \"GRANT CREATE ON SCHEMA public TO nw_cabinet;\""
+## Start Nemesida WAF API service
+mv /etc/nginx/conf.d/nwaf-api.conf.disabled /etc/nginx/conf.d/nwaf-api.conf
+nginx -t && service nginx reload
+(netstat -lnp | grep -q ':8080') || (echo -e "\033[0;101mERROR: start Nemesida WAF API is failed\033[0m"; exit 1)
+systemctl restart nw-api rldscupd nginx memcached
