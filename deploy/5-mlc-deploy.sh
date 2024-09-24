2192f1085ff32abfc09e5aa976681d454cdbb090
Hekp0 <95239339+Hekp0@users.noreply.github.com>
2024-09-24 16:41:32 +0300

    Update 5-mlc-deploy.sh

--- deploy/5-mlc-deploy.sh
+++ deploy/5-mlc-deploy.sh
@@ -2,7 +2,7 @@
 
 ##
 # Example of use:
-# /bin/bash ./5-mlc-deploy.sh 'nwaf_lic_key=xxx' 'api_srv_ip=x.x.x.x' 'fn_srv_ip=x.x.x.x' 'fn_rmq_user=xxx' 'fn_rmq_pwd=x.x.x.x' 'sys_proxy=xxx:xx' 'api_proxy=xxx:xx'
+# /bin/bash ./5-mlc-deploy.sh 'nwaf_lic_key=xxx' 'api_srv_ip=x.x.x.x' 'dyn_srv_ip=x.x.x.x' 'dyn_rmq_user=xxx' 'dyn_rmq_pwd=x.x.x.x' 'sys_proxy=xxx:xx' 'api_proxy=xxx:xx'
 ##
 
 ## OS detection
@@ -16,11 +16,6 @@ then
   exit 1
 fi
 
-## RabbitMQ environment
-rabbitmq_asc_url=$(curl https://www.rabbitmq.com/docs/install-rpm#red-hat-8-centos-stream-8-modern-fedora-releases -A 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0' | grep -E '?https://github.com/rabbitmq/rabbitmq-server/releases/download/' | awk -F '["]' '{print $4}')
-rabbitmq_rpm_url=$(curl https://www.rabbitmq.com/docs/install-rpm#red-hat-8-centos-stream-8-modern-fedora-releases -A 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0' | grep -E '?https://github.com/rabbitmq/rabbitmq-server/releases/download/' | awk -F '["]' '{print $2}')
-rabbitmq_rpm_name=$(curl https://www.rabbitmq.com/docs/install-rpm#red-hat-8-centos-stream-8-modern-fedora-releases -A 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0' | grep -E '?https://github.com/rabbitmq/rabbitmq-server/releases/download/' | awk -F '["]' '{print $2}' | awk -F [/] '{print $9}')
-
 ## Processing the params
 for i in "$@"; do
   case $i in
@@ -32,16 +27,16 @@ for i in "$@"; do
       api_srv_ip="${i#*=}"
       shift
       ;;
-    fn_srv_ip=*)
-      fn_srv_ip="${i#*=}"
+    dyn_srv_ip=*)
+      dyn_srv_ip="${i#*=}"
       shift
       ;;
-    fn_rmq_user=*)
-      fn_rmq_user="${i#*=}"
+    dyn_rmq_user=*)
+      dyn_rmq_user="${i#*=}"
       shift
       ;;
-    fn_rmq_pwd=*)
-      fn_rmq_pwd="${i#*=}"
+    dyn_rmq_pwd=*)
+      dyn_rmq_pwd="${i#*=}"
       shift
       ;;
     sys_proxy=*)
@@ -60,16 +55,16 @@ done
 ## Parameters validation
 if [ -z "$nwaf_lic_key" ]; then echo -e "\033[0;101mERROR: nwaf_lic_key parameter is missing\033[0m" ; exit 1 ; fi
 if [ -z "$api_srv_ip" ]; then echo -e "\033[0;101mERROR: api_srv_ip parameter is missing\033[0m" ; exit 1 ; fi
-if [ -z "$fn_srv_ip" ]; then echo -e "\033[0;101mERROR: fn_srv_ip parameter is missing\033[0m" ; exit 1 ; fi
-if [ -z "$fn_rmq_user" ]; then echo -e "\033[0;101mERROR: fn_rmq_user parameter is missing\033[0m" ; exit 1 ; fi
-if [ -z "$fn_rmq_pwd" ]; then echo -e "\033[0;101mERROR: fn_rmq_pwd parameter is missing\033[0m" ; exit 1 ; fi
+if [ -z "$dyn_srv_ip" ]; then echo -e "\033[0;101mERROR: dyn_srv_ip parameter is missing\033[0m" ; exit 1 ; fi
+if [ -z "$dyn_rmq_user" ]; then echo -e "\033[0;101mERROR: dyn_rmq_user parameter is missing\033[0m" ; exit 1 ; fi
+if [ -z "$dyn_rmq_pwd" ]; then echo -e "\033[0;101mERROR: dyn_rmq_pwd parameter is missing\033[0m" ; exit 1 ; fi
 
 ## Display the applied parameters
 echo "Nemesida WAF license key: $nwaf_lic_key"
 echo "Nemesida WAF API server IP: $api_srv_ip"
-echo "Filtering node server IP: $fn_srv_ip"
-echo "RabbitMQ user for connection to the filtering node: $fn_rmq_user"
-echo "RabbitMQ password for connection to the filtering node: $fn_rmq_pwd"
+echo "Filtering node server IP: $dyn_srv_ip"
+echo "RabbitMQ user for connection to the filtering node: $dyn_rmq_user"
+echo "RabbitMQ password for connection to the filtering node: $dyn_rmq_pwd"
 echo "System proxy (if used): $sys_proxy"
 echo "Nemesida WAF API proxy (if used): $api_proxy"
 
@@ -143,6 +138,10 @@ fi
 
 echo "Setting up Nemesida AI MLC"
 
+rabbitmq_asc_url=$(curl https://www.rabbitmq.com/docs/install-rpm#red-hat-8-centos-stream-8-modern-fedora-releases -A 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0' | grep -E 'https://github.com/rabbitmq/rabbitmq-server/releases/download/' | awk -F '["]' '{print $4}')
+rabbitmq_rpm_url=$(curl https://www.rabbitmq.com/docs/install-rpm#red-hat-8-centos-stream-8-modern-fedora-releases -A 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0' | grep -E 'https://github.com/rabbitmq/rabbitmq-server/releases/download/' | awk -F '["]' '{print $2}')
+rabbitmq_rpm_name=$(curl https://www.rabbitmq.com/docs/install-rpm#red-hat-8-centos-stream-8-modern-fedora-releases -A 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0' | grep -E 'https://github.com/rabbitmq/rabbitmq-server/releases/download/' | awk -F '["]' '{print $2}' | awk -F [/] '{print $9}')
+
 if [[ "$os_base" == debian ]]
 then
   apt-get install -qqy python3 python3-venv python3-pip python3-dev python3-setuptools libc6-dev rabbitmq-server gcc memcached
@@ -191,7 +190,7 @@ sed -i "s|nwaf_license_key = |nwaf_license_key = $nwaf_lic_key|" /opt/mlc/mlc.co
 sed -i "s|sys_proxy = |sys_proxy = $sys_proxy|" /opt/mlc/mlc.conf
 sed -i "s|api_proxy = |api_proxy = $api_proxy|" /opt/mlc/mlc.conf
 sed -r "s|localhost|$api_srv_ip|" /opt/mlc/mlc.conf
-sed -r "s|rmq_host = guest:guest@127.0.0.1|rmq_host = $fn_rmq_user:$fn_rmq_pwd@$fn_srv_ip|" /opt/mlc/mlc.conf
+sed -r "s|rmq_host = guest:guest@127.0.0.1|rmq_host = $dyn_rmq_user:$dyn_rmq_pwd@$dyn_srv_ip|" /opt/mlc/mlc.conf
 
 ## Start Nemesida AI MLC service
 systemctl restart mlc_main rabbitmq-server memcached
