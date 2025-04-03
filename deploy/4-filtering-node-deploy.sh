52beb122472a196fd6422345477501598f8ae038
Hekp0 <95239339+Hekp0@users.noreply.github.com>
2025-04-03 11:10:12 +0300

    Delete Ubuntu focal

--- deploy/4-filtering-node-deploy.sh
+++ deploy/4-filtering-node-deploy.sh
@@ -103,7 +103,7 @@ then
 elif [[ "$os_base" == ubuntu ]]
 then
   apt-get update -qqy
-  if [[ "$os_code_name" =~ focal|jammy ]]
+  if [[ "$os_code_name" == jammy ]]
   then
     echo "deb [arch=amd64] https://nemesida-security.com/repo/nw/ubuntu $os_code_name non-free" > /etc/apt/sources.list.d/NemesidaWAF.list
   elif [[ "$os_code_name" == noble ]]
@@ -175,10 +175,7 @@ then
   apt-get install -qqy nwaf-dyn-$nginx_version
 elif [[ "$os_base" == ubuntu ]]
 then
-  if [[ "$os_code_name" == focal ]]
-  then
-    apt-get install -qqy python3.9 python3.9-venv python3-pip python3.9-dev python3-setuptools libcurl3-gnutls librabbitmq4 libcurl4-openssl-dev libc6-dev gcc g++ memcached rabbitmq-server
-  elif [[ "$os_code_name" =~ jammy|noble ]]
+  if [[ "$os_code_name" =~ jammy|noble ]]
   then
     apt-get install -qqy python3 python3-venv python3-pip python3-dev python3-setuptools libcurl3-gnutls librabbitmq4 libcurl4-openssl-dev libc6-dev gcc g++ memcached rabbitmq-server
   fi
